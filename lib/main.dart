a9a624355ecaf591be44da64f39cba18a4d8a87f
Michael <mguastal@umich.edu>
2024-02-01 11:29:52 -0800

    initial set up for nfc functionality, may need to add some more permissions for ios version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
+import 'package:ndef/ndef.dart' as ndef;
 
 void main() {
   runApp(MyApp());
@@ -8,67 +13,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
+      home: MyHomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String nfcData = 'No NFC data';
+
+  @override
+  void initState() {
+    super.initState();
+    _initNFC();
+  }
+
+  Future<void> _initNFC() async {
+    try {
+      var availability = await FlutterNfcKit.nfcAvailability;
+      if (availability != NFCAvailability.available) {
+        //print something
+      }
+
+      var tag = await FlutterNfcKit.poll(timeout: Duration(days:1, seconds: 10),
+          iosMultipleTagMessage: "Multiple tags found!", iosAlertMessage: "Scan your tag");
+
+
+      print(jsonEncode(tag));
+      bool isNdefAvailable = tag.ndefAvailable ?? false;
+      // read NDEF records if available
+      if (isNdefAvailable) {
+        for (var record in await FlutterNfcKit.readNDEFRecords(cached: false)) {
+          setState(() {
+                if(record is ndef.TextRecord) {
+                  nfcData = record.text ?? "";
+                }
+          });
+        }
+      }
+    } on Exception catch (e) {
+      print('Error initializing NFC: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    FlutterNfcKit.finish();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+        title: Text('NFC Reader'),
       ),
       body: Center(
-        child: Image.asset(
-          'assets/tapup_logo.png',
-          fit: BoxFit.cover, // Adjust the fit as needed
-          // You can set width and height as well, if you want to resize the logo
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              nfcData,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
         ),
       ),
     );
